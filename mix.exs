929c3c799d6fd664acbc0caf0022a62f0d702df1
Marcin Białoń <marcin@bialon.net>
2015-09-20 22:41:43 +0200

    Mix config

--- mix.exs
+++ mix.exs
@@ -7,26 +7,28 @@ defmodule Excoap.Mixfile do
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description,
+     package: package,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     []
+  end
+
+  defp description do
+    """
+    CoAP implementation for Elixir
+    """
+  end
+
+  defp package do
+    [contributors: ["Marcin Białoń"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/mbialon/excoap"}]
   end
 end
